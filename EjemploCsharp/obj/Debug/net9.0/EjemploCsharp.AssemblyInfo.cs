8be31d6fa31fd7357699122def5acb80e3eeba7f
Ricardo <ricardoh345.00@hotmail.com>
2025-07-15 19:09:16 -0400

    first

--- EjemploCsharp/obj/Debug/net9.0/EjemploCsharp.AssemblyInfo.cs
+++ EjemploCsharp/obj/Debug/net9.0/EjemploCsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjemploCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+171f6a28da8f8be7d9a77c35d33de97a646c92ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjemploCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjemploCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
